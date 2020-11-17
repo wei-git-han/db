44e7b2a3c5bd735fbb73cc85c725c74228076ea0
Administrator <892664088@qq.com>
2020-11-17 15:13:22 +0800

    代码提交

--- src/main/resources/static/app/db/document/djlr/js/djlr_view.js
+++ src/main/resources/static/app/db/document/djlr/js/djlr_view.js
@@ -61,11 +61,11 @@ var pageModule = function(){
 			data:{infoId:fileId},
 			success:function(data){
 				if(data && !!data){
-					$(".blfkchangeH").attr("style","position:absolute;top:34px;left:0;bottom:0;width:100%;right:0;")
+					$(".blfkchangeH").attr("style","position:absolute;top:50px;left:0;bottom:0;width:100%;right:0;")
 					//$("#ifcuibanContent").text(data.userName+"催办："+data.urgeContent).attr("title",data.userName+"催办："+data.urgeContent).show();
 					$.each(data,function(index,item){
 					$("#ifcuibanContent").show()
-					$("#ifcuibanContent").append('<div>'+item.userName+"催办："+item.urgeContent+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+item.createdTime+'</div>');
+					$("#ifcuibanContent").append('<div class="overText" title="'+item.userName+"催办："+item.urgeContent+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+item.createdTime+'">'+item.userName+"催办："+item.urgeContent+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+item.createdTime+'</div>');
 				})
 				}
 			}
