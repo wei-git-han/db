38a87fd10c32631f63926868c0951154ed96ac55
sunzh <824562329@qq.com>
2020-11-30 18:22:05 +0800

    调用远程接口

--- src/main/java/com/css/app/db/business/controller/PersonDocumentController.java
+++ src/main/java/com/css/app/db/business/controller/PersonDocumentController.java
@@ -3,7 +3,6 @@ package com.css.app.db.business.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +13,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.css.addbase.apporgmapped.entity.BaseAppOrgMapped;
 import com.css.addbase.apporgmapped.service.BaseAppOrgMappedService;
 import com.css.addbase.constant.AppConstant;
-import com.css.app.db.business.entity.DocumentFile;
 import com.css.app.db.business.service.DocumentFileService;
-import com.css.app.db.business.service.DocumentInfoService;
 import com.css.base.utils.CrossDomainUtil;
 import com.css.base.utils.Response;
-import com.css.base.utils.UUIDUtils;
 import com.github.pagehelper.PageHelper;
 
 @RestController
@@ -28,8 +24,6 @@ public class PersonDocumentController {
 
 	@Autowired
 	private BaseAppOrgMappedService baseAppOrgMappedService;
-	@Autowired
-	private DocumentInfoService documentInfoService;
 	@Autowired
 	private DocumentFileService documentFileService;
 
@@ -50,7 +44,7 @@ public class PersonDocumentController {
 		map.put("search", search);
 
 		PageHelper.startPage(page, rows);
-		BaseAppOrgMapped mapped = (BaseAppOrgMapped) baseAppOrgMappedService.orgMappedByOrgId("", "",
+		BaseAppOrgMapped mapped = (BaseAppOrgMapped) baseAppOrgMappedService.orgMappedByOrgId("", "root",
 				AppConstant.APP_GWZB);
 		if (mapped == null) {
 			return;
@@ -68,28 +62,25 @@ public class PersonDocumentController {
 	}
 	
 	/**
-	 * 列表 呈报列表
+	 * 保存文件
 	 * 
-	 * @param page，第几页
-	 * @param pagesize，每页条数
 	 */
 	@ResponseBody
 	@RequestMapping("/saveFile")
-	public void saveFile(String[] fileIds) {
-		JSONObject result=new JSONObject();
-		DocumentFile file = null;
-		for(String oldId : fileIds){
-			if (StringUtils.isNotEmpty(oldId)) {
-				
-				file = documentFileService.queryObject(oldId);
-				file.setId(UUIDUtils.random());
-				documentFileService.save(file);
-			}
+	public void saveFile(String fileIds,String idpdf) {
+		LinkedMultiValueMap<String, Object> infoMap = new LinkedMultiValueMap<String, Object>();
+		BaseAppOrgMapped mapped = (BaseAppOrgMapped) baseAppOrgMappedService.orgMappedByOrgId("", "root",
+				AppConstant.APP_GWZB);
+		String url = mapped.getUrl() + "/app/zhms/gwzb/saveFile";
+		infoMap.add("fileIds", fileIds);
+		infoMap.add("idpdf", idpdf);
+		infoMap.add("sort", String.valueOf(documentFileService.queryMinSort(idpdf)));
+		JSONObject retInfo = CrossDomainUtil.getJsonData(url, infoMap);
+		if(retInfo!=null) {
+			Response.json(retInfo);
+		}else {
+			Response.json("result", "fail");
 		}
-		result.put("id", file.getId());
-		result.put("result", "success");
-		
-		Response.json(result);
 	}
 
 }
